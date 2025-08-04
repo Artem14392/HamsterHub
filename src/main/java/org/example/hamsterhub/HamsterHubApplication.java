c9308e4f47db8b2842f39a952f9f4ffe858b3508
Artem <ortexvain3900@mail.ru>
2025-08-04 14:56:19 +0300

    first commit

---
+++ src/main/java/org/example/hamsterhub/HamsterHubApplication.java
@@ -0,0 +1,13 @@
+package org.example.hamsterhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamsterHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamsterHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hamsterhub/HamsterHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hamsterhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsterHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
